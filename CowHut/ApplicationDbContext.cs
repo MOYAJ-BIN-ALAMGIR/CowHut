5d89306c7ce5ebd617a6e30aaaa24a4f6b2d053b
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-09 13:03:11 +0600

    Add project files.

---
+++ CowHut/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CowHut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CowHut
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ CowHut/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CowHut.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string PhoneNumber {get;set;}
+        public string Role {get;set;}
+        public string Password {get;set;}
+        public string Name {get;set;}
+        public string Address {get;set;}
+}
+}
