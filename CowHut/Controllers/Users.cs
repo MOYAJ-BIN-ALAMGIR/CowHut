2d2a0a194b44234dcf57e2258dd281f54ef6bcfa
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-09 17:44:52 +0600

    implemented signup,showall, update, delete, search

---
+++ CowHut/Controllers/Users.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CowHut.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CowHut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+        public Users(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        [HttpPost("signup")]
+        public IActionResult CreateUser(User user)
+        {
+            db.users.Add(user);
+            db.SaveChanges();
+            return Ok(new
+            {
+                success = true,
+                StatusCode = 200,
+                message = "user created successfully",
+                data = user
+            });
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var user = db.users.ToList();
+            return Ok(new
+            {
+                success = true,
+                StatusCode = 200,
+                messag = "Users retrived successfull",
+                data = user 
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = db.users.Find(id);
+            if(user == null)
+            {
+                return NotFound(new
+                {
+                    success = false,
+                    statuscode = 404,
+                    message = "user not found"
+                });
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+            return Ok(new
+            {
+                success = true,
+                StatusCode = 200,
+                message = "user deleted successfully",
+                data = user
+            });
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateUser(int id, User newuser)
+        {
+            var user = db.users.Find(id);
+            if(user == null)
+            {
+                return NotFound(new
+                {
+                    success = false,
+                    StatusCode = 404,
+                    message = "User not found"
+                });
+            }
+
+            user.PhoneNumber = newuser.PhoneNumber ?? user.PhoneNumber;
+            user.Role = newuser.Role ?? user.Role;
+            user.Password = newuser.Password ?? user.Password;
+            user.Name = newuser.Name ?? user.Name;
+            user.Address = newuser.Address ?? user.Address;
+
+            db.SaveChanges();
+
+            return Ok(new
+            {
+                success = true,
+                StatusCode = 200,
+                message = "User Updated Successfully",
+                data = user
+            });
+        }
+
+        [HttpGet("search")]
+        public IActionResult SearchUser(
+            string ? phonenumber,
+            string ? name,
+            string ? address,
+            string ? role)
+        {
+            var query = db.users.AsQueryable();
+
+            if(!string.IsNullOrEmpty(phonenumber))
+            {
+                query = query.Where(u => u.PhoneNumber == phonenumber);
+            }
+            if(!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(u => u.Name == name);
+            }
+            if (!string.IsNullOrEmpty(address))
+            {
+                query = query.Where(u => u.Address == address);
+            }
+            if (!string.IsNullOrEmpty(role))
+            {
+                query = query.Where(u => u.Role == role);
+            }
+            var result = query.ToList();
+            return Ok(new
+            {
+                success = true,
+                StatusCode = 200,
+                total = result.Count,
+                data = result
+            });
+        }
+    }
+}
--- CowHut/Program.cs
+++ CowHut/Program.cs
@@ -1,5 +1,6 @@
 using CowHut;
 using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -21,6 +22,22 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseExceptionHandler(errorAPP =>
+{
+    errorAPP.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+        var error = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        await context.Response.WriteAsJsonAsync(new
+        {
+            success = false,
+            message = error?.Message,
+            stack = error?.StackTrace
+        });
+    });
+});
 
 app.UseHttpsRedirection();
 
